8c25cf7ad8ec36bdf947b7b8cbf093d44fc13830
the1812 <the1812@outlook.com>
2023-02-15 09:31:46 +0800

    Improve search results style

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -45,11 +45,11 @@ const canSearch = computed(() => !busy && Boolean(keyword))
     <div class="flex flex-col gap-4 my-auto">
       <div class="text-2xl font-medium self-center">Doujin Meta</div>
       <div class="flex items-center justify-center gap-3">
-        <InputText type="text" class="flex-grow max-w-screen-md" v-model="keyword" @keydown.enter="handleSearch" placeholder="Album name" />
-        <Button :loading="busy" icon="pi pi-search" :disabled="!canSearch" @click="handleSearch" label="Search" />
+        <InputText type="text" class="flex-grow min-w-0 max-w-[700px]" v-model="keyword" @keydown.enter="handleSearch" placeholder="Album name" />
+        <Button class="shrink-0" :loading="busy" icon="pi pi-search" :disabled="!canSearch" @click="handleSearch" label="Search" />
       </div>
     </div>
-    <div class="h-0 flex-grow overflow-auto self-stretch flex flex-col gap-1">
+    <div class="h-0 w-full flex-grow overflow-auto flex flex-col gap-1 self-center max-w-[800px]">
       <RouterLink v-for="item of searchResult" :key="item.id" :to="`/albums/${item.name}/${item.id}`">
         <AlbumSearchItem :item="item" />
       </RouterLink>
