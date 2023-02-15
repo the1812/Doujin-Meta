e2df4dbda2c130626e34b4f5f87552d263abf84f
the1812 <the1812@outlook.com>
2023-02-15 09:13:32 +0800

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,36 @@ import { RouterView } from 'vue-router'
 
 <template>
   <div class="flex flex-col">
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Transition :name="(route.meta.transition as string) || 'fade'">
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
   </div>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.slide-left-enter-from,
+.slide-left-leave-to {
+  transform: translateX(-24px);
+  opacity: 0;
+}
+.slide-right-enter-from,
+.slide-right-leave-to {
+  transform: translateX(24px);
+  opacity: 0;
+}
+</style>
--- src/routes.ts
+++ src/routes.ts
@@ -3,8 +3,8 @@ import Detail from './pages/Detail.vue'
 import Home from './pages/Home.vue'
 
 const routes: RouteRecordRaw[] = [
-  { path: '/', component: Home },
-  { path: '/albums/:name/:id', component: Detail },
+  { path: '/', component: Home, meta: { transition: 'slide-left' } },
+  { path: '/albums/:name/:id', component: Detail, meta: { transition: 'slide-right' } },
 ]
 
 export const router = createRouter({
