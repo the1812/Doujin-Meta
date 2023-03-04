5756d607333a20f1675d17cdbc78efe89f5e1332
the1812 <the1812@outlook.com>
2023-03-04 11:02:14 +0800

    Refactor themes

---
+++ src/theme.ts
@@ -0,0 +1,27 @@
+const withDarkMode = (updater: (isDarkMode: boolean) => void | Promise<void>) => {
+  return () => {
+    const query = window.matchMedia('(prefers-color-scheme: dark)')
+    updater(query.matches)
+    query.addEventListener('change', e => {
+      updater(e.matches)
+    })
+  }
+}
+
+export const initAppIcon = withDarkMode((isDarkMode) => {
+  const iconLink = document.querySelector('link[rel="icon"]') as HTMLLinkElement
+  iconLink.href = `/images/Logo${isDarkMode ? '.White' : ''}.svg`
+})
+
+// export const initAppTheme = withDarkMode(async isDarkMode=> {
+//   const styleElement = document.getElementById('theme-styles') as HTMLStyleElement
+//   if (isDarkMode) {
+//     const { default: style } = await import('primevue/resources/themes/lara-dark-purple/theme.css')
+//     styleElement.dataset.theme = 'dark'
+//     styleElement.innerHTML = style
+//   } else {
+//     const { default: style } = await import('primevue/resources/themes/lara-light-indigo/theme.css')
+//     styleElement.dataset.theme = 'light'
+//     styleElement.innerHTML = style
+//   }
+// })
