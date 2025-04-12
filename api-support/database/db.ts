bbb1890ba125abdcc418c1d4784fa7df76cf0918
the1812 <the1812@outlook.com>
2025-04-12 18:15:30 +0800

    Add db type log

--- api-support/database/db.ts
+++ api-support/database/db.ts
@@ -14,11 +14,14 @@ const getRequiredEnv = (key: string) => {
 
 const getDatabaseUrl = () => {
   if (process.env.IS_LOCAL === '1') {
+    console.log('db type', 'local')
     return getRequiredEnv('POSTGRES_LOCAL_URL')
   }
   if (process.env.IS_PREVIEW === '1') {
+    console.log('db type', 'development')
     return getRequiredEnv('POSTGRES_DEVELOPMENT_URL')
   }
+  console.log('db type', 'production')
   return getRequiredEnv('POSTGRES_URL')
 }
 
